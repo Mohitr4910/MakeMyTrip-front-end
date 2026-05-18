0f0df4606f752a70e025cdfc9a683377bf7b73ec
Mohitr4910 <mohitrahangdale67890@gmail.com>
2026-05-18 15:15:48 +0530

    payment error

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../Untils/axiosInstance";
+import "./Payment.css";
 
 
 let Payment = () => {
